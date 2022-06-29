d94245d98019ae4b4bbcdedd166b3a818a7ebb42
Kharak kunwar <kharak.kunwar2@gmail.com>
2022-06-29 16:48:30 +0530

    Add files via upload

---
+++ Fibonacci Sequence with the help of funtions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 29 16:34:20 2022
+
+@author: khara
+"""
+
+def feb(n):
+    a=0
+    b=1
+    if n==1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,n):
+            c=a+b
+            a=b
+            b=c
+            print(c)
+            
+feb(12)
