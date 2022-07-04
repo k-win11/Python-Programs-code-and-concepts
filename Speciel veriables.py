8e32dec8cf24d151c7de923f091fb22639019fff
Kharak kunwar <kharak.kunwar2@gmail.com>
2022-07-04 09:10:46 +0530

    Add files via upload

---
+++ Speciel veriables.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  4 08:46:39 2022
+
+@author: khara
+"""
+
+#Speciel veriables (__name__) and(__main__)
+
+# it can be used for to check wether the current
+#script is being run on its own or being imported
+#somewhere else by combining it with if statement as
+
+if __name__=='__main__':
+    print('file being run direct')
+else:
+    ('file being imported')
+    
+
+ 
+
