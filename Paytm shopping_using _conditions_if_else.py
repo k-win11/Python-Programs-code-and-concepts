af860b3da14f354f0da31fe929b3698026f53f9d
Kharak kunwar <87529000+k-win11@users.noreply.github.com>
2023-09-09 11:14:24 +0530

    Add files via upload

---
+++ Paytm shopping_using _conditions_if_else.py
@@ -0,0 +1,19 @@
+# Paytm Discount offer 
+
+print('''Offer on sele Amount more then 6000, gain 50% flate discount''')
+print('Also offer on card use icici as code icici get 10% flet extra discount')
+card=str('enter the card')
+amount=float(input('enter the amount: '))
+card=str('enter the icici card code: ')
+if amount>=5000:
+    print('total amout pay',amount*0.5)
+    card=str(input('enter the card: '))
+    if card=='icici':
+        print('final mount is: ',amount*0.5*.9)
+else:
+    print('total payamout: ',amount)
+print('Thanks for shopping')
+
+    
+  
+
